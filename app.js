b38ad6be6badecb8008190a2e9c387dabc5f8c7b
Joey Pagaduan <joey.paga220@gmail.com>
2023-05-03 20:37:44 -0700

    Finished Step 5, Part 3: styling the init. route

--- app.js
+++ app.js
@@ -1,7 +1,45 @@
 const express = require("express");
+const morgan = require("morgan");
+const postBank = require("./postBank");
+const path = require('path');
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(morgan('dev'));
+
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+
+  const html = 
+  `<!DOCTYPE html>
+    <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <div class="news-list">
+        <header><img src="/logo.png"/>Wizard News</header>
+        ${posts.map(post => `
+          <div class='news-item'>
+            <p>
+              <span class="news-position">${post.id}. ▲</span>
+              ${post.title}
+              <small>(by ${post.name})</small>
+            </p>
+            <small class="news-info">
+              ${post.upvotes} upvotes | ${post.date}
+            </small>
+          </div>`
+        ).join('')}
+      </div>
+    </body>
+  </html>`
+
+  res.send(html);
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = 1337;
 
